ab3837347a3ec7d512ad259f61ceed67a5f8688a
Philippe Murphy <philippe.murphy.1@ulaval.ca>
2024-09-10 18:46:47 -0400

    Etape 2

--- GestionDuneClinique/src/main/java/org/example/Clinic.java
+++ GestionDuneClinique/src/main/java/org/example/Clinic.java
@@ -6,68 +6,59 @@ public class Clinic {
 
     private List<Patient> doctorsQ = new ArrayList<>();
     private List<Patient> radiologyQ = new ArrayList<>();
+    private TriageType triageType;
 
-    public Patient meetPatient()
+    public Clinic(TriageType triageType)
+    {
+        this.triageType = triageType;
+    }
+
+    public Patient doctorMeetPatient()
     {
         return doctorsQ.removeFirst();
     }
+    public Patient radiologyMeetPatient()
+    {
+        return radiologyQ.removeFirst();
+    }
 
-    public boolean docstorsQIsEmpty()
+    public boolean doctorsQIsEmpty()
     {
         return doctorsQ.isEmpty();
     }
+
     public boolean radiologyQIsEmpty()
     {
         return radiologyQ.isEmpty();
     }
 
-    public Clinic(/* ... */) {
-    }
-
-    public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {
-        // TODO
-    }
+    public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {}
 
     private void addPatientToDoctorsQ(Patient patient)
     {
-        doctorsQ.add(patient);
+        if(this.triageType == TriageType.FIFO){
+            doctorsQ.add(patient);
+        }
+        if(this.triageType == TriageType.GRAVITY){
+            doctorsQ.addFirst(patient);
+        }
     }
+
     private void addPatientToRadiologyQ(Patient patient)
     {
         radiologyQ.add(patient);
     }
+
     public void addPatient(Patient patient)
     {
-        addPatientToDoctorsQ(patient);
-        if(patient.getSymptom().equals(VisibleSymptom.BROKEN_BONE) || patient.getSymptom().equals(VisibleSymptom.SPRAIN)){
-            addPatientToRadiologyQ(patient);
+        this.addPatientToDoctorsQ(patient);
+        if (patient.getSymptom().equals(VisibleSymptom.BROKEN_BONE) || patient.getSymptom().equals(VisibleSymptom.SPRAIN)) {
+            this.addPatientToRadiologyQ(patient);
         }
     }
-
-
 
     public boolean isEmpty()
     {
         return doctorsQ.isEmpty();
     }
-
-
-    // D'autres méthodes peuvent être nécessaires
 }
-
-/*
-Scénario 1: Celui où un patient se présente à la clinique avec une migraine
-Lorsqu'un patient se présente avec MIGRAINE (peu importe la gravité)
-Alors il est le premier dans la file d'attente du médecin
-Et il n'est pas dans la file d'attente de la radiologie
-
-Scénario 2: Celui où 2 patients se présentent à la clinique
-Étant donné qu'il y a un patient dans la file d'attente du médecin
-Lorsqu'un patient se présente avec FLU (peu importe la gravité)
-Alors il est le deuxième dans la file d'attente du médecin
-Et il n'est pas dans la file d'attente de la radiologie
-
-Scénario 3: Celui où un patient se présente à la clinique avec une entorse
-Lorsqu'un patient se présente avec SPRAIN (peu importe la gravité)
-Alors il est le premier dans la file d'attente du médecin
-Et il est la première dans la file d'attente de la radiologie*/
--- GestionDuneClinique/src/test/java/ClinicTest.java
+++ GestionDuneClinique/src/test/java/ClinicTest.java
@@ -1,5 +1,6 @@
 import org.example.Clinic;
 import org.example.Patient;
+import org.example.TriageType;
 import org.example.VisibleSymptom;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,7 @@ public class ClinicTest {
     @Test
     public void givenAPatient_whenAddingPatientInDoctorsQ_thenPatientAdded()
     {
-        Clinic clinic = new Clinic();
+        Clinic clinic = new Clinic(TriageType.FIFO);
         Patient patient = new Patient("Jean-Guy", 4, VisibleSymptom.COLD);
 
         clinic.addPatient(patient);
@@ -21,64 +22,76 @@ public class ClinicTest {
     @Test
     public void givenOnePatientInList_whenMeet_thenPatientIsRemoved()
     {
-        Clinic clinic = new Clinic();
+        Clinic clinic = new Clinic(TriageType.FIFO);
         Patient patient = new Patient("Jean-Guy", 4, VisibleSymptom.COLD);
 
         clinic.addPatient(patient);
-        clinic.meetPatient();
+        clinic.doctorMeetPatient();
 
-        assertTrue(clinic.docstorsQIsEmpty());
+        assertTrue(clinic.doctorsQIsEmpty());
     }
-
 
     @Test
     public void givenTwoPatientsInList_whenMeet_thenPatientRemovedInSameOrder()
     {
-        Clinic clinic = new Clinic();
+        Clinic clinic = new Clinic(TriageType.FIFO);
         Patient patient = new Patient("Jean-Guy", 4, VisibleSymptom.COLD);
         Patient patient2 = new Patient("Jean-Marie", 4, VisibleSymptom.COLD);
         clinic.addPatient(patient);
         clinic.addPatient(patient2);
 
-        Patient meetedPatient = clinic.meetPatient();
-        Patient meetedPatient2 = clinic.meetPatient();
+        Patient meetedPatient = clinic.doctorMeetPatient();
+        Patient meetedPatient2 = clinic.doctorMeetPatient();
 
         assertEquals(patient, meetedPatient);
         assertEquals(patient2, meetedPatient2);
-        assertTrue(clinic.docstorsQIsEmpty());
+        assertTrue(clinic.doctorsQIsEmpty());
+    }
+
+    @Test
+    public void givenRadiologyQueueNotEmpty_whenradiologyMeetPatient_thenFisrtPatientFromRadiologyQueueRemoved()
+    {
+        Clinic clinic = new Clinic(TriageType.FIFO);
+        Patient patient = new Patient("Jean-Guy", 4, VisibleSymptom.BROKEN_BONE);
+        Patient patient2 = new Patient("Jean-Marie", 4, VisibleSymptom.BROKEN_BONE);
+        clinic.addPatient(patient);
+        clinic.addPatient(patient2);
+
+        Patient meetedPatient = clinic.radiologyMeetPatient();
+        Patient meetedPatient2 = clinic.radiologyMeetPatient();
+
+        assertEquals(patient, meetedPatient);
+        assertEquals(patient2, meetedPatient2);
+        assertTrue(clinic.radiologyQIsEmpty());
     }
 
 
     @Test
     public void givenPatientHasBrokenBone_whenAddingPatient_thenAddToBothDoctorAndRadiologyQ()
     {
-        Clinic clinic = new Clinic();
+        Clinic clinic = new Clinic(TriageType.FIFO);
         Patient patient = new Patient("Jean-Guy", 4, VisibleSymptom.BROKEN_BONE);
         clinic.addPatient(patient);
 
-        assertFalse(clinic.docstorsQIsEmpty());
+        assertFalse(clinic.doctorsQIsEmpty());
         assertFalse(clinic.radiologyQIsEmpty());
     }
 
+    @Test
+    public void givenPatientHasHighGravityAndAlgoIsGravity_whenAddingPatient_thenPatientFirstInQueue()
+    {
+        Clinic clinic = new Clinic(TriageType.GRAVITY);
+        Patient patient = new Patient("Jean-Guy", 4, VisibleSymptom.BROKEN_BONE);
+        Patient patient2 = new Patient("BOB", 7, VisibleSymptom.COLD);
+        clinic.addPatient(patient);
+        clinic.addPatient(patient2);
 
+        Patient meetedPatient = clinic.doctorMeetPatient();
+        Patient meetedPatient2 = clinic.doctorMeetPatient();
 
+        assertEquals(patient2, meetedPatient);
+        assertEquals(patient, meetedPatient2);
+        assertTrue(clinic.doctorsQIsEmpty());
+    }
 }
 
-
-/*
-Scénario 1: Celui où un patient se présente à la clinique avec une migraine
-Lorsqu'un patient se présente avec MIGRAINE (peu importe la gravité)
-Alors il est le premier dans la file d'attente du médecin
-Et il n'est pas dans la file d'attente de la radiologie
-
-Scénario 2: Celui où 2 patients se présentent à la clinique
-Étant donné qu'il y a un patient dans la file d'attente du médecin
-Lorsqu'un patient se présente avec FLU (peu importe la gravité)
-Alors il est le deuxième dans la file d'attente du médecin
-Et il n'est pas dans la file d'attente de la radiologie
-
-Scénario 3: Celui où un patient se présente à la clinique avec une entorse
-Lorsqu'un patient se présente avec SPRAIN (peu importe la gravité)
-Alors il est le premier dans la file d'attente du médecin
-Et il est la première dans la file d'attente de la radiologie
-*/
